6b3f9a04108065579d00fb17e9d91dda9476c10e
Indi Chen <indichen@etusolution.com>
2017-02-15 14:54:36 +0800

    Update version to 0.5.

--- module/setup.py
+++ module/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='etunexus',
-      version='1.0',
+      version='0.5',
       description='Etu Nexus API',
       url='http://www.etunexus.com/',
       author='Etu Corporation',
-      author_email='',
+      author_email='indichen@etusolution.com',
       license='GPL',
       packages=find_packages(),
       install_requires=[
