88aa166349b51201d0544b7862d23e236245aa14
Fin <finchen@etusolution.com>
2017-11-01 16:22:38 +0800

    Update version to 0.6.8.

--- module/etunexus/__init__.py
+++ module/etunexus/__init__.py
@@ -3,8 +3,8 @@
 API_VERSION = 'v3'
 API_USER_AGENT = 'Etu Nexus API Python Client' + '-' + API_VERSION
 
-__version__ = '0.6.6'
+__version__ = '0.6.8'
 __url__ = 'http://www.etunexus.com/'
-__author_email__ = 'indichen@etusolution.com'
+__author_email__ = 'support@etusolution.com, finchen@etusolution.com'
 __author__ = 'Etu Corporation'
 __license__ = 'GPL'
